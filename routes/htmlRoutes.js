7c073be7911b71c338c74f025cfcb80f6afcd273
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-05-16 14:16:03 -0400

    4th test

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = function(app){
+
+    app.get('/index', function(req, res){
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    })
+
+    app.get('/notes', function(req, res){
+        res.sendFile(path.join(__dirname, '../public/notes.html'))
+    
+    })
+
+    app.get('/api/notes', function(req,res){
+        res.sendFile(path.join(__dirname, '../db/db.json'))
+    })
+
+    app.get("/api/notes/:notes", function(req, res) {
+        var chosen = req.params.notes.id;
+      
+        console.log(chosen);
+      
+    //     for (var i = 0; i < notes.length; i++) {
+    //       if (chosen === notes[i].routeName) {
+    //         return res.json(notes[i]);
+    //       }
+    //     }
+      
+    //     return res.json(false);
+      });
+      
+
+    app.get('*', function(req, res){
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    })
+
+  
+}
