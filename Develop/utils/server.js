eb63b2a734a7c41a2814eb33834384749967cabe
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-05-15 20:09:59 -0400

    testing to submit to heroku

---
+++ Develop/utils/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'))
+
+
+const apiRoutes = require("../routes/apiRoutes")(app);
+const htmlRoutes = require("../routes/htmlRoutes")(app);
+
+
+
+PORT = 3005;
+
+
+
+app.listen(PORT, console.log(`Server is listening on port: ${PORT}`))
